6aa42efb5d59bc6de088e047849714b60f7e35d2
George <georgejoj28@gmail.com>
2024-05-17 18:10:17 +0300

    update migrations

--- facilities/admin.py
+++ facilities/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import HealthFacilities
 
-# Register your models here.
+admin.site.register(HealthFacilities, admin.ModelAdmin)
--- facilities/models.py
+++ facilities/models.py
@@ -1,8 +1,12 @@
 from django.contrib.gis.db import models
 
 class HealthFacilities(models.Model):
-    name = models.CharField(max_length=80)
-    healthcare = models.CharField(max_length=100)
-    amenity = models.CharField(max_length=80)
-    operator:type = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, null=True)
+    healthcare = models.CharField(max_length=100, null=True)
+    amenity = models.CharField(max_length=80, null=True)
+    operator:type = models.CharField(max_length=80, null=True)
     geom = models.MultiPointField(srid=4326)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
